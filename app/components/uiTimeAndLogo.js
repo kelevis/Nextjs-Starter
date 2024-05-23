dd5c0650f287f5d85d95e136ba39dfa4ef6db382
tree <lushuwu@chinamobile.com>
2024-05-23 18:13:45 +0800

    -

--- app/components/uiTimeAndLogo.js
+++ app/components/uiTimeAndLogo.js
@@ -1,11 +1,10 @@
 // 在 JavaScript 和 TypeScript 中，导入模块时有两种主要方式：默认导入和命名导入。这两种方式有不同的语法和用途。
-
 // "use client"
 import timeStyles  from '@/app/styles/time.module.css';
-import reactLogoStyles  from '@/app/styles/Logo.module.css';
-import Image from 'next/image'; // 引入 next/image 组件
-// import logo from '@/app/components/logo.svg'; // 导入 SVG 图像文件
-import Reactimage from "@/app/components/RotaIcon"
+// import reactLogoStyles  from '@/app/styles/Logo.module.css';
+// import Image from 'next/image'; // 引入 next/image 组件
+// import Reactimage from "@/app/components/RotaIcon"
+
 import React, { useState, useEffect } from 'react';
 import Logo from "@/app/components/uiReactLogo"
 
