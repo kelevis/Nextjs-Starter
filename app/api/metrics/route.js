6471298f6ec1215aa4859aa74589bc717162081c
tree <lushuwu@chinamobile.com>
2024-08-14 18:20:54 +0800

    -

--- app/api/metrics/route.js
+++ app/api/metrics/route.js
@@ -4,24 +4,39 @@ import {NextResponse} from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
-const filePath = path.resolve(process.cwd(), 'data/pageviews.json');
+// const filePath = path.resolve(process.cwd(), 'data/pageviews.json');
+git
+let pageviews = {
+    count: 0,
+    lastVisit: null
+};
+
+// Function to increment the count and update the last visit
+function incrementPageviews() {
+    pageviews.count = (pageviews.count || 0) + 1;
+    pageviews.lastVisit = new Date().toISOString();
+}
+
 
 export async function GET(request) {
 
     const token = request.cookies.get('token')   //
 
-    const data = fs.readFileSync(filePath, 'utf8');
-    const pageviews = JSON.parse(data);
-    pageviews.count = (pageviews.count || 0) + 1;
-    pageviews.lastVisit = new Date().toISOString();
-    // Write the updated count back to the file
-    fs.writeFileSync(filePath, JSON.stringify(pageviews, null, 2), 'utf8');
+    // const data = fs.readFileSync(filePath, 'utf8');
+    // const pageviews = JSON.parse(data);
+    // pageviews.count = (pageviews.count || 0) + 1;
+    // pageviews.lastVisit = new Date().toISOString();
+    // // Write the updated count back to the file
+    // fs.writeFileSync(filePath, JSON.stringify(pageviews, null, 2), 'utf8');
 
+// Usage example
+    incrementPageviews();
+    console.log(pageviews);
 
-    console.log("filePath:", filePath)
-    console.log("data:", data)
-    console.log("pageviews", pageviews)
-    console.log("pageviews.count", pageviews.count)
+    // console.log("filePath:", filePath)
+    // console.log("data:", data)
+    // console.log("pageviews", pageviews)
+    // console.log("pageviews.count", pageviews.count)
 
     // Default values for other metrics
     const metrics = {
